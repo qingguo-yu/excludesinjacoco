b88091eb6721f91dbc1fed42e494fd2246d16559
Qingguo Yu <qiyu@ebay.com>
2017-04-30 22:07:37 -0700

    add project

---
+++ src/main/java/com/jacoco/notincludes/App1.java
@@ -0,0 +1,15 @@
+package com.jacoco.notincludes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App1 
+{
+	private final String prop1 = "Hello World from App1!";
+	
+    public String getProp1() {
+		return prop1;
+	}
+
+}
---
+++ src/main/java/com/jacoco/testexcludes/App.java
@@ -0,0 +1,19 @@
+package com.jacoco.testexcludes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private final String prop1 = "Hello World!";
+	
+    public String getProp1() {
+		return prop1;
+	}
+
+	public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
